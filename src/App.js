31cd97766726bd0b69929e90f7fd83e57dc2dcb9
Benjamin Jordan <benjamin@thegoldenmule.com>
2023-01-25 14:22:47 -0600

    Tons of updates.

--- src/App.js
+++ src/App.js
@@ -24,25 +24,50 @@ const typeToIcon = {
   'writing': () => <PenFill />,
 };
 
-const ElementFactory = ({ type, date, title, subtitle, description, url, imageUrl, }) => {
+const contentStyleFor = ({ type, category }) => {
+  if (category === 'career') {
+    return { outline: '2px solid #daa520', borderLeft: '20px solid #daa520', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 };
+  }
+
+  if (category === 'publications') {
+    return { border: '2px dashed #ccc', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 };
+  }
+
+  if (category === 'contributions') {
+    return { border: '4px double #ccc', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 };
+  }
+
+  if (category === 'products') {
+    return { outline: '1px solid #ccc', borderBottom: '20px solid #daa520', borderRadius:'16px 14px 8px 40px', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 };
+  }
+
+  return { outline: '1px solid #ccc', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 };
+};
+
+const ElementFactory = ({ category, type, date, title, subtitle, description, url, imageUrl, }) => {
   return (
     <VerticalTimelineElement
-      contentStyle={{ border: '1px solid #ccc', color: '#000', fontFamily: '"Antic Didone"', fontWeight: 500 }}
-      contentArrowStyle={{ borderRight: '9px solid #ccc' }}
+      className={`type-${type} category-${category}`}
+      contentStyle={contentStyleFor({ type, category })}
+      contentArrowStyle={{ borderRight: '9px solid #daa520' }}
       dateClassName="date"
       iconStyle={{ background: '#daa520', color: '#fff' }}
       icon={typeToIcon[type]()}
       date={date}
     >
-      <h3>{title}</h3>
-      {subtitle && <h5>{subtitle}</h5>}
+      <div className="d-flex flex-column pb-4">
+        <h3>{title}</h3>
+        {subtitle && <h5>{subtitle}</h5>}
+      </div>
       <div className="d-flex align-items-center">
-        <div className='pe-4'>
-          <img className='rounded' src={imageUrl} width={150} />
-        </div>
+        { imageUrl && (
+          <div className='pe-4'>
+            <img className='rounded' src={imageUrl} width={150} />
+          </div>
+        )}
         <div className="d-flex flex-column justify-content-between">
           <p className='m-0 pb-2'>{description}</p>
-          <a target="_blank" href={url}>Read More</a>
+          { url && <a target="_blank" href={url}>Read More</a> }
         </div>
       </div>
     </VerticalTimelineElement>
@@ -57,6 +82,7 @@ const TimelineFilter = ({ value, setValue }) => (
       <DropdownItem onClick={() => setValue('Publications')}>Publications</DropdownItem>
       <DropdownItem onClick={() => setValue('Career')}>Career</DropdownItem>
       <DropdownItem onClick={() => setValue('Products')}>Products</DropdownItem>
+      <DropdownItem onClick={() => setValue('Contributions')}>Contributions</DropdownItem>
     </DropdownButton>
   </div>
 );
