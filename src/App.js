c177e3cc0dfcd9f889ab62ecc89d5a239a414484
Benjamin Jordan <benjamin@thegoldenmule.com>
2023-01-25 18:58:34 -0600

    Hamfisted fixes for style on mobile.

--- src/App.js
+++ src/App.js
@@ -63,7 +63,8 @@ const ElementFactory = ({ category, type, date, title, subtitle, description, ur
       <div className="d-flex align-items-center">
         { imageUrl && (
           <div className='pe-4'>
-            <img className='rounded' src={imageUrl} width={150} />
+            <img className='d-lg-none rounded' src={imageUrl} width={75} />
+            <img className='d-lg-block d-none rounded' src={imageUrl} width={150} />
           </div>
         )}
         <div className="d-flex flex-column justify-content-between">
@@ -114,6 +115,31 @@ const Timelines = ({ filter, }) => {
   );
 };
 
+const ExternalLinks = () => (
+  <Col className="d-flex mb-4">
+    <div className="m-auto">
+      <a className="px-2" target="_blank" href="https://thegoldenmule.medium.com/">
+        <img width={32} src="https://thegoldenmule.com/thumbnails/medium-logo.svg" />
+      </a>
+      <a className="px-2" target="_blank" href="https://github.com/thegoldenmule">
+        <img width={32} src="https://thegoldenmule.com/thumbnails/github-mark.svg" />
+      </a>
+      <a className="px-2" target="_blank" href="https://www.linkedin.com/in/benjaminmicahj/">
+        <img width={32} src="https://thegoldenmule.com/thumbnails/linkedin-logo.png" />
+      </a>
+      <a className="px-2" target="_blank" href="https://www.goodreads.com/user/show/106890887-benjamin-jordan">
+        <img width={32} src="https://thegoldenmule.com/thumbnails/goodreads-logo.png" />
+      </a>
+      <a className="px-2" target="_blank" href="https://thegoldenmule.com/blog">
+        <img width={32} src="https://thegoldenmule.com/thumbnails/wordpress-logo.png" />
+      </a>
+      <a className="px-2" target="_blank" href="https://thegoldenmule.svbtle.com/">
+        <PencilFill />
+      </a>
+    </div>
+  </Col>
+);
+
 const queryClient = new QueryClient();
 
 function App() {
@@ -132,18 +158,6 @@ function App() {
 
   return (
     <div className="App">
-      {/*
-        <Col>
-          <div className="d-flex flex-column fst-italic">
-            <a target="_blank" href="https://thegoldenmule.medium.com/">Technical Writing</a>
-            <a target="_blank" href="https://github.com/thegoldenmule">GitHub</a>
-            <a target="_blank" href="https://www.linkedin.com/in/benjaminmicahj/">LinkedIn</a>
-            <a target="_blank" href="https://thegoldenmule.svbtle.com/">Creative Writing</a>
-            <a target="_blank" href="https://www.goodreads.com/user/show/106890887-benjamin-jordan">Reading List</a>
-            <a target="_blank" href="https://thegoldenmule.com/blog">Technical Writing Archives</a>
-          </div>
-        </Col>
-      */}
       <QueryClientProvider client={queryClient}>
         <Container>
         <Row className="pt-4">
@@ -152,42 +166,20 @@ function App() {
             </Col>
           </Row>
           <Row>
-            <Col className="d-flex mb-4">
-              <div className="m-auto">
-                <a className="px-2" target="_blank" href="https://thegoldenmule.medium.com/">
-                  <img width={32} src="https://thegoldenmule.com/thumbnails/medium-logo.svg" />
-                </a>
-                <a className="px-2" target="_blank" href="https://github.com/thegoldenmule">
-                  <img width={32} src="https://thegoldenmule.com/thumbnails/github-mark.svg" />
-                </a>
-                <a className="px-2" target="_blank" href="https://www.linkedin.com/in/benjaminmicahj/">
-                  <img width={32} src="https://thegoldenmule.com/thumbnails/linkedin-logo.png" />
-                </a>
-                <a className="px-2" target="_blank" href="https://www.goodreads.com/user/show/106890887-benjamin-jordan">
-                  <img width={32} src="https://thegoldenmule.com/thumbnails/goodreads-logo.png" />
-                </a>
-                <a className="px-2" target="_blank" href="https://thegoldenmule.com/blog">
-                  <img width={32} src="https://thegoldenmule.com/thumbnails/wordpress-logo.png" />
-                </a>
-                <a className="px-2" target="_blank" href="https://thegoldenmule.svbtle.com/">
-                  <PencilFill />
-                </a>
-              </div>
-            </Col>
+            <ExternalLinks />
           </Row>
           <Row className="py-4">
             <Col className="d-flex pt-4">
-              <img width={400} className="m-auto" src="https://thegoldenmule.com/thumbnails/card.jpg" />
+              <img className="d-block d-md-none m-auto" style={{ width: '100%' }} src="https://thegoldenmule.com/thumbnails/card.jpg" />
+              <img className="d-none d-md-block m-auto" style={{ width: '400px' }} src="https://thegoldenmule.com/thumbnails/card.jpg" />
             </Col>
           </Row>
-
-          
-
-          
-          
           <Row>
-            <Col className="d-flex">
-              <div className="m-auto d-flex">
+            <Col className="d-flex m-auto">
+              <div className="m-auto d-none d-md-flex">
+                {buttons}
+              </div>
+              <div className="m-auto d-block d-md-none">
                 {buttons}
               </div>
             </Col>
