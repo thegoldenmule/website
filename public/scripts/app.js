177da654a1f807119dabf91dfd6ae2809d6d8787
Benjamin Jordan <benjamin@thegoldenmule.com>
2024-11-10 08:31:38 -0600

    fixed some layering and added ship motion trail

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,22 @@
-import { Application, Graphics } from "./pixi.mjs";
+import { Application, Container, Graphics } from "./pixi.mjs";
+
+const backgroundColor = "#222222";
 
 const createShip = () => {
   // tiangle ship, like asteroids
   const size = 10;
+  const halfSize = size / 2;
+  const color = 0xffffff;
   const ship = new Graphics()
-    .setStrokeStyle({ color: 0xffffff, width: 1 })
-    .moveTo(size, 0)
-    .lineTo(-size, 10)
-    .lineTo(-size / 2, 0)
-    .lineTo(-size, -size)
-    .lineTo(size, 0)
-    .stroke();
+    .setStrokeStyle({ color, width: 1 })
+    .setFillStyle({ color })
+    .moveTo(size + halfSize, 0)
+    .lineTo(-size + halfSize, 10)
+    .lineTo(0, 0)
+    .lineTo(-size + halfSize, -size)
+    .lineTo(size + halfSize, 0)
+    .stroke()
+    .fill();
 
   return ship;
 };
@@ -40,6 +46,37 @@ const addShipMovement = (app, ship, targetPosition) => {
     // apply friction
     velocity.x *= friction;
     velocity.y *= friction;
+  });
+};
+
+const addShipMotionTrails = (app, ship) => {
+  const shipPositions = [];
+  const trailLength = 100;
+
+  const trail = new Graphics();
+  ship.parent.addChild(trail);
+
+  app.ticker.add(() => {
+    shipPositions.push({ x: ship.x, y: ship.y });
+
+    if (shipPositions.length > trailLength) {
+      shipPositions.shift();
+    }
+
+    trail
+      .clear()
+      .moveTo(ship.x, ship.y)
+      .setStrokeStyle({ color: 0xffffff, width: 1, alpha: 1 });
+
+    for (let i = shipPositions.length - 1; i >= 0; i--) {
+      const position = shipPositions[i];
+      trail
+        .lineTo(position.x, position.y)
+        .stroke()
+        .setStrokeStyle({ color: 0xffffff, width: 1, alpha: i / trailLength });
+    }
+
+    trail.stroke();
   });
 };
 
@@ -87,13 +124,22 @@ const addParallaxBackgrounds = (app, ship) => {
 
 (async () => {
   const app = new Application();
-  await app.init({ background: "#222222", resizeTo: window });
+  await app.init({
+    background: backgroundColor,
+    resizeTo: window,
+    preference: "webgl",
+    antialias: true,
+    autoDensity: true,
+    resolution: window.devicePixelRatio || 1,
+    powerPreference: "high-performance",
+  });
   document.body.appendChild(app.canvas);
 
   const ship = createShip();
   ship.x = app.screen.width / 2;
   ship.y = app.screen.height / 2;
-  app.stage.addChild(ship);
+  const shipLayer = new Container();
+  shipLayer.addChild(ship);
 
   // follow the mouse
   app.stage.eventMode = "dynamic";
@@ -108,8 +154,14 @@ const addParallaxBackgrounds = (app, ship) => {
   // ship movement
   addShipMovement(app, ship, targetPosition);
 
+  // add ship motion trails
+  addShipMotionTrails(app, ship);
+
   // add parallax background
   const layers = addParallaxBackgrounds(app, ship);
+
+  // ship always on top
+  app.stage.addChild(shipLayer);
 
   const res = await fetch("/timeline.json");
   const { events } = await res.json();
@@ -126,6 +178,7 @@ const addParallaxBackgrounds = (app, ship) => {
 
     const asteroid = new Graphics()
       .setStrokeStyle({ color: layer.color, width: 1 })
+      .setFillStyle({ color: backgroundColor })
       .moveTo(x, y);
 
     for (let i = 0; i < points; i++) {
@@ -145,7 +198,7 @@ const addParallaxBackgrounds = (app, ship) => {
       }
     }
 
-    asteroid.closePath().stroke();
+    asteroid.closePath().stroke().fill();
     layer.addChild(asteroid);
   });
 })();
